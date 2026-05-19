aead83cb31e000da057e1ed8ab5e80ca2ede61ad
payal nijave <nijavepayal8855gmail.com>
2026-05-19 17:26:43 +0530

    Add Project Files

---
+++ routes/review.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+
+const Review = require("../models/review.js");
+const Listing = require("../models/listing.js");
+const wrapAsync = require("../utils/wrapAsync");
+
+const { validateReview, isLoggedIn, isReviewAuthor } =require("./middleware.js");
+const reviewController = require("../controllers/reviews.js");
+
+// reviews post route
+router.post("/",
+    isLoggedIn,
+    validateReview,
+    wrapAsync(reviewController.createReview)
+);
+
+// delete review route
+router.delete("/:reviewId",
+    isLoggedIn,
+    isReviewAuthor,
+    wrapAsync(reviewController.destroyReview)
+);
+
+module.exports = router;
