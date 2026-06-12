5e7bbf53f04b52012cbc4429549a3910f0e1d725
payal nijave <nijavepayal3176@gmail.com>
2026-06-12 23:48:30 +0530

    Remove hardcoded Mapbox token

--- public/js/map.js
+++ public/js/map.js
@@ -1,16 +1,8 @@
-// mapboxgl.accessToken=mapToken;
-
-//     const map = new mapboxgl.Map({
-//         // accessToken: 'pk.eyJ1IjoicGF5YWxuaWphdmUiLCJhIjoiY21xMXV3NXF2MGR6ODJzczZraXBtc3V0ZSJ9.1gKeKRUTjbIW1MspnLbEFQ',
-//         container: 'map', // container ID
-//         style:"mapbox://styles/mapbox/streets-v12",
-//         center:[77.2090,28.6139 ],  // starting position [lng, lat]. Note that lat must be set between -90 and 90
-//         zoom: 9 // starting zoom
-//     });
 
 
     const map = new mapboxgl.Map({
-        accessToken: 'pk.eyJ1IjoicGF5YWxuaWphdmUiLCJhIjoiY21xMXhuaG4wMDBudTJwcjFheW5jNGd1ZCJ9.LN_IuYJpbbocJ9OU65m7_w',
+// accessToken="pk.eyJ1IjoicGF5YWxuaWphdmUiLCJhIjoiY21xMXhuaG4wMDBudTJwcjFheW5jNGd1ZCJ9.LN_IuYJpbbocJ9OU65m7_w",
+accessToken=mapToken,
         container: 'map', // container ID
         center: listing.geometry.coordinates,// starting position [lng, lat]. Note that lat must be set between -90 and 90
         zoom: 9 // starting zoom
@@ -21,3 +13,23 @@ const marker = new mapboxgl.Marker({color:"red"})
 setPopup(new mapboxgl
     .Popup({offset:25}).setHTML(`<p><b>${listing.title}</b><i> (Exact location will be provided after booking)</i></p>`))
     .addTo(map);
+
+
+// mapboxgl.accessToken = mapToken;
+
+// const map = new mapboxgl.Map({
+//     container: "map",
+//     style: "mapbox://styles/mapbox/streets-v12",
+//     center: listing.geometry.coordinates,
+//     zoom: 9
+// });
+
+// const marker = new mapboxgl.Marker({ color: "red" })
+//     .setLngLat(listing.geometry.coordinates)
+//     .setPopup(
+//         new mapboxgl.Popup({ offset: 25 })
+//             .setHTML(
+//                 `<p><b>${listing.title}</b><br>(Exact location will be provided after booking)</p>`
+//             )
+//     )
+//     .addTo(map);
